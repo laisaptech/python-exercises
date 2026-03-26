3b8e365d3c2b8db56bcbb6788df4601581847d94
Lais Carvalho <laisap.dev@gmail.com>
2026-03-26 13:47:54 -0300

    Add exercise 01

---
+++ exercise01.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+weight = input("Enter your weight: ")
+
+print(f"Hello, {name}!")
+print(f"You are {age} years old and weigh {weight} kg.")
